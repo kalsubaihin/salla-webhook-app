422ab7447df0cdbcaf8d7f6010681b72658dcd11
kalsubaihin <khaled@hawa.co>
2024-12-25 19:23:58 +0300

    simple example now

--- app.py
+++ app.py
@@ -1,19 +1,7 @@
-from flask import Flask, request
-import json
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Hello, Heroku!"
-
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    print('In webhook')
-    data = request.json  # Parse the JSON payload
-    print(data)  # Log the data for debugging
-    # Process data and push to BigQuery
-    return "Received", 200
-
-if __name__ == "__main__":
-    app.run(int(os.environ.get('PORT', 5000)))
+def index():
+    return "Hello World!"
