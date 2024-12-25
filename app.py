acc507118de80c23874ac1a9cc24951b499cc2dc
kalsubaihin <khaled@hawa.co>
2024-12-25 19:32:36 +0300

    dssad

--- app.py
+++ app.py
@@ -3,5 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "Hello World!"
+def home():
+    return "Hello World sss!"
+
+if __name__ == '__main__':
+    app.run()
